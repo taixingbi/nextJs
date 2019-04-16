773316fdf1e4737d9a1366fdf91e23baa2ec990b
taixingbi <bitaihang@gmail.com>
2019-04-16 15:51:07 -0400

    Update cart.js

--- pages/shopping/cart.js
+++ pages/shopping/cart.js
@@ -1,32 +1,61 @@
+import React, {Component} from "react";
 import Layout from '../../components/MyLayout.js'
 
-export default function Cart() {
-  this.state= "test";
+//import * as post from '../rest/post';
 
-  return (
-    <Layout>
+class Cart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      first_name: '',
+      last_name: ''
+    }
 
-      <h1> Shoping cart</h1>
-      <p>  {this.state}  </p>
-      <form>
-        <label>
-          Email:
-          <input type="text" name="name" />
-        </label>
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        <label>
-          First Name:
-          <input type="text" name="name" />
-        </label>
 
-        <label>
-        Last Name:
-          <input type="text" name="name" />
-        </label>
 
-        <input type="submit" value="Submit" />
-      </form>
+  async handleSubmit(event) {
+    event.preventDefault();
+    
 
-    </Layout>
-  )
+    await post.postPersonalInfo(this.state.email, this.state.first_name, this.state.last_name);
+
+    alert("post successfully");
+
+  }
+
+  changeHandler = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <form onSubmit={this.handleSubmit}>
+
+          <label htmlFor="email">Email</label>
+          <input value={this.state.email}  name="email" type="email" onChange={this.changeHandler}/>
+
+          <label >First Name</label>
+          <input value={this.state.first_name}  name="first_name" type="text" onChange={this.changeHandler}/>
+
+          <label htmlFor="last_name">Last Name</label>
+          <input value={this.state.last_name}  name="last_name" type="text" onChange={this.changeHandler}/>
+
+          <button>Submit</button>
+        </form>
+
+      </Layout>
+
+    );
+  }
 }
+
+export default Cart;
